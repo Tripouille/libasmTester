1b7ca7e737028455293f2f3c30cb7e6e8fd41d33
jgambard <jmichel.gambard@gmail.com>
2020-12-20 16:24:50 +0100

    add list_remove_if

--- libasm.h
+++ libasm.h
@@ -36,7 +36,7 @@ int			ft_atoi_base(char *str, char *base);
 void		ft_list_push_front(t_list **begin_list, void *data);
 int			ft_list_size(t_list *begin_list);
 void		ft_list_sort(t_list **begin_list, int (*cmp)(void*, void*));
-void		ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(),
+void		ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(void*, void*),
 								void (*free_fct)(void *));
 
 #endif
--- tests/ft_list_remove_if_test.cpp
+++ tests/ft_list_remove_if_test.cpp
@@ -10,46 +10,71 @@ extern "C"
 
 int iTest = 1;
 
-void wrap_list_sort(t_list **begin_list, int (*cmp)(void*, void*))
+void wrap_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(void*, void*),
+								void (*free_fct)(void *))
 {
 	int tripouille[42];
-	ft_list_sort(begin_list, cmp);
+	ft_list_remove_if(begin_list, data_ref, cmp, free_fct);
 	tripouille[41] = 42;
 }
 
-int lower(void *d1, void *d2)
-{return ((long long)d1 > (long long)d2);}
+int equal(void *d1, void *d2)
+{return (*(char*)d1 - *(char*)d2);}
+
+void remove(void *s)
+{
+	*(char*)s = 'X';
+}
 
 int main(void)
 {
 	signal(SIGSEGV, sigsegv);
-	CATEGORY("ft_list_sort")
+	CATEGORY("ft_list_remove_if")
 
 	t_list * list = NULL;
-	wrap_list_sort(&list, &lower);
+	char s[100] = {0}; char c = 0; char ref = 0;
+	for (int i = 0; i < 10; ++i)
+		s[i] = i + '0';
+	wrap_list_remove_if(&list, &c, &equal, &remove);
 	/* 1 */ check(list == NULL);
 
-	ft_list_push_front(&list, (void*)1);
-	wrap_list_sort(&list, &lower);
-	/* 2 */ check(list->data == (void*)1);
+	ft_list_push_front(&list, &c);
+	wrap_list_remove_if(&list, &ref, &equal, &remove);
+	/* 2 */ check(list == NULL && c == 'X' && ref == 0);
 
-	ft_list_push_front(&list, (void*)2);
-	ft_list_push_front(&list, (void*)3);
-	ft_list_push_front(&list, (void*)4);
-	ft_list_push_front(&list, (void*)-1);
-	ft_list_push_front(&list, (void*)5);
-	ft_list_push_front(&list, (void*)-2);
-	ft_list_push_front(&list, (void*)6);
-	ft_list_push_front(&list, (void*)-3);
-	ft_list_push_front(&list, (void*)7);
-	ft_list_push_front(&list, (void*)0);
-	wrap_list_sort(&list, &lower); t_list * tmp = list; string result;
+	for (int i = 9; i >= 0; --i)
+		ft_list_push_front(&list, s + i);
+
+	ref = '0';
+	wrap_list_remove_if(&list, &ref, &equal, &remove);
+	t_list * tmp = list; string result;
 	while(tmp != NULL)
 	{
-		result += to_string((long long)tmp->data);
+		result += *(char*)tmp->data;
 		tmp = tmp->next;
 	}
-	/* 3 */ check(result == string("-3-2-101234567"));
+	/* 3 */ check(result == string("123456789") && s[0] == 'X' && ref == '0');
+
+
+	ref = '9';
+	wrap_list_remove_if(&list, &ref, &equal, &remove);
+	tmp = list; result = "";
+	while(tmp != NULL)
+	{
+		result += *(char*)tmp->data;
+		tmp = tmp->next;
+	}
+	/* 4 */ check(result == string("12345678") && s[9] == 'X' && ref == '9');
+
+	ref = '5';
+	wrap_list_remove_if(&list, &ref, &equal, &remove);
+	tmp = list; result = "";
+	while(tmp != NULL)
+	{
+		result += *(char*)tmp->data;
+		tmp = tmp->next;
+	}
+	/* 5 */ check(result == string("1234678") && s[5] == 'X' && ref == '5');
 	lst_clean(&list);
 	cout << ENDL;
 	return (0);
