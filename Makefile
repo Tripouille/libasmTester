eb2ff49594ed19086144463b995a56c26a6bf7cf
jgambard <jmichel.gambard@gmail.com>
2020-12-20 11:50:10 +0100

    add strdup

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .DEFAULT_GOAL	:= m
 UTILS			= utils/sigsegv.cpp utils/color.cpp utils/check.cpp
 TESTS_PATH		= tests/
-MANDATORY		= strlen strcpy strcmp write read
+MANDATORY		= strlen strcpy strcmp write read strdup
 VMANDATORY		= $(addprefix v, $(MANDATORY))
 BONUS			= lstnew lstadd_front lstsize lstlast lstadd_back lstdelone lstclear lstiter lstmap
 VBONUS			= $(addprefix v, $(BONUS))
--- tests/ft_strdup_test.cpp
+++ tests/ft_strdup_test.cpp
@@ -1,27 +1,42 @@
 extern "C"
 {
 #define new tripouille
-#include "libft.h"
+#include "libasm.h"
 #undef new
 }
 
 #include "sigsegv.hpp"
 #include "check.hpp"
 #include <string.h>
+#include <unistd.h>
+
 
 int iTest = 1;
+
+char * wrap_strdup(const char *s1)
+{
+	int tripouille[42];
+	char * r = ft_strdup(s1);
+	tripouille[41] = 42;
+	return (r);
+}
+
 int main(void)
 {
 	signal(SIGSEGV, sigsegv);
 	cout << FG_LGRAY << "ft_strdup\t: "; cout.flush();
 
-	char * s = ft_strdup((char*)"coucou");
-	/* 1 */ check(!strcmp(s, "coucou"));
-	/* 2 */ mcheck(s, strlen("coucou") + 1); free(s);
-	
-	s = ft_strdup((char*)"");
-	/* 3 */ check(!strcmp(s, ""));
-	/* 4 */ mcheck(s, 1); free(s);
+	char const * src = ""; char * s = wrap_strdup(src); 
+	/* 1 */ check(!strcmp(s, src));
+	/* 2 */ mcheck(s, strlen(src) + 1); free(s);
+
+	src = "1"; s = wrap_strdup(src); 
+	/* 3 */ check(!strcmp(s, src));
+	/* 4 */ mcheck(s, strlen(src) + 1); free(s);
+
+	src = "123"; s = wrap_strdup(src); 
+	/* 5 */ check(!strcmp(s, src));
+	/* 6 */ mcheck(s, strlen(src) + 1); free(s);
 	cout << ENDL;
 	return (0);
 }
